4d235b1fd2bb2a481f1fbef6f01b7137029490df
Steve Dignam <steve@dignam.xyz>
2021-03-16 17:43:47 -0400

    fix(config): _RetryDict should be `total=False` (#7)
    Added the missing properties previously, neglected to update to make
    it `total=False`

--- tests/test_boto.py
+++ tests/test_boto.py
@@ -1,8 +1,11 @@
 import boto3
+from botocore.config import Config
 
 
 def test_get_object_body() -> None:
-    s3 = boto3.client("s3")
+    s3 = boto3.client(
+        "s3", config=Config(retries={"mode": "standard", "max_attempts": 10})
+    )
     obj = s3.get_object(Bucket="", Key="")
     assert "Body" in obj
     data = obj["Body"].read()
